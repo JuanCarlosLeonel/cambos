c27259ba9cac5dad022ec0dcaa58d17d37f11bd8
tonystefanlog <tonystefanlog@hotmail.comm>
2020-12-18 08:43:58 -0300

    modelagem

--- cambos/core/models.py
+++ cambos/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_currentuser.db.models import CurrentUserField
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Setor(models.Model):
+    DIVISAO_CHOICES = (
+            ('Têxtil', 'Têxtil'),
+            ('Confecção', 'Confecção'),             
+        )
+    nome    = models.CharField(max_length=20)
+    divisao = models.CharField(max_length=20, choices=DIVISAO_CHOICES)
+
+
+class User(AbstractUser):
+    setor = models.ForeignKey(Setor, null=True, blank=True, on_delete=models.SET_NULL)
