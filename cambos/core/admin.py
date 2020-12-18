e4c4da67f1f12b979dcbcaf7bfdfe5487adc08fe
tonystefanlog <tonystefanlog@hotmail.comm>
2020-12-18 09:37:42 -0300

    modelagem3

--- cambos/core/admin.py
+++ cambos/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import (
     User,
+    Setor
 )
 
+admin.site.register(Setor)
 admin.site.register(User)
--- cambos/core/models.py
+++ cambos/core/models.py
@@ -10,6 +10,12 @@ class Setor(models.Model):
     nome    = models.CharField(max_length=20)
     divisao = models.CharField(max_length=20, choices=DIVISAO_CHOICES)
 
+    def __str__(self):
+        return f'{self.nome}'
+    
+    class Meta:
+        verbose_name_plural = "Setores"
+
 
 class User(AbstractUser):
     setor = models.ForeignKey(Setor, null=True, blank=True, on_delete=models.SET_NULL)
