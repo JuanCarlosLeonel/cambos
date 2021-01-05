9625a4ecc2a73d00e38b9fd351b50a00014a2669
tonystefanlog <tonystefanlog@hotmail.comm>
2021-01-05 19:36:57 -0300

    chart1

--- cambos/static/js/chart-data.js
+++ cambos/static/js/chart-data.js
@@ -1,47 +1,78 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
-	
-	var lineChartData = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				label: "My First dataset",
-				fillColor : "rgba(220,220,220,0.2)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			},
-			{
-				label: "My Second dataset",
-				fillColor : "rgba(48, 164, 255, 0.2)",
-				strokeColor : "rgba(48, 164, 255, 1)",
-				pointColor : "rgba(48, 164, 255, 1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(48, 164, 255, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			}
-		]
+// Produção
 
-	}
+var producaoChart = {
+	labels : labels1,
+	datasets : [
 		
-	var barChartData = {
-		labels : myLabels,
-		datasets : [
-			
-			{	
-				fillColor : "rgba(48, 164, 255, 0.2)",
-				strokeColor : "rgba(48, 164, 255, 0.8)",
-				highlightFill : "rgba(48, 164, 255, 0.75)",
-				highlightStroke : "rgba(48, 164, 255, 1)",
-				data : myData,
-			}
-		]
+		{	
+			fillColor : "rgba(48, 164, 255, 0.2)",
+			strokeColor : "rgba(48, 164, 255, 0.8)",
+			highlightFill : "rgba(48, 164, 255, 0.75)",
+			highlightStroke : "rgba(48, 164, 255, 1)",
+			data : data1,
+		}
+	]
 
-	} 
+} 
+var chart1 = document.getElementById("chartProducao").getContext("2d");
+var producao = new Chart(chart1).Line(producaoChart, {responsive: true});
 
+
+// Composição do custo
+
+var composicaoChart = {
+	labels : labels1,
+	datasets : [
+		{
+			label: "My First dataset",
+			fillColor : "rgba(220,220,220,0.2)",
+			strokeColor : "rgba(220,220,220,1)",
+			pointColor : "rgba(220,220,220,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data : data1
+		},
+		{
+			label: "My Second dataset",
+			fillColor : "rgba(48, 164, 255, 0.2)",
+			strokeColor : "rgba(48, 164, 255, 1)",
+			pointColor : "rgba(48, 164, 255, 1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(48, 164, 255, 1)",
+			data : data1
+		}
+	]	
+}
+
+var composicaoOptions = {
+	legend: {
+		display: true,
+		labels: {
+			fontColor: "rgb(255, 99, 132)"
+		}
+	},
+	tooltips: {
+		mode: 'index',
+		intersect: false,
+	},
+	responsive:true,
+	scales: {
+		xAxes: [{ stacked: true }],
+		yAxes: [{ stacked: true }]
+	  }	  
+}
+
+var chart2 = document.getElementById("chartComposicao").getContext("2d");
+var composicao = new Chart(chart2,).Bar(composicaoChart, composicaoOptions);
+
+
+
+	
+	
+		
+	
 	var pieData = [
 			{
 				value: 300,
