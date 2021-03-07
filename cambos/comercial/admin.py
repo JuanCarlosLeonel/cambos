89d2ff29bf1c7d49ef20dbb370db70606e07d266
tonystefanlog <tonystefanlog@hotmail.comm>
2021-03-07 19:57:51 -0300

    comercial_model2

--- cambos/comercial/admin.py
+++ cambos/comercial/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import (
     Comercial,
+    Cliente,
+    CustoManipulacao,
+    Cotacao,
+    Tecido,
+    PrecoTecido,
+    PontuacaoProducao,
+    Produto,
+    ItemCotacao,
+    Proposta,
 )
 
 admin.site.register(Comercial)
+admin.site.register(Cliente)
+admin.site.register(CustoManipulacao)
+admin.site.register(Cotacao)
+admin.site.register(Tecido)
+admin.site.register(PrecoTecido)
+admin.site.register(PontuacaoProducao)
+admin.site.register(Produto)
+admin.site.register(ItemCotacao)
+admin.site.register(Proposta)
 
 
 # Register your models here.
