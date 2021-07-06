07a06fa4c12d3a855e4ddc63587a2c7acf2895d3
tonystefanlog <tonystefanlog@hotmail.comm>
2021-07-06 20:28:05 -0300

    merge_juan

--- cambos/scheduler/scheduler.py
+++ cambos/scheduler/scheduler.py
@@ -8,7 +8,7 @@ from pytz import utc
 
 def start():
     scheduler = BackgroundScheduler()                
-    #scheduler.add_job(bot.iniciar)  
+    scheduler.add_job(bot.iniciar)  
     scheduler.add_job(update_api, 'interval', minutes=5)       
     #scheduler.add_job(bot3.send_message, 'cron', day_of_week='mon-fri', hour='8', minute='00', name='botRotina', start_date='2021-05-16')        
     scheduler.start()    
