6b6a8806afad51a6678da879f12bb9134e21ea9a
tonystefanlog <tonystefanlog@hotmail.comm>
2021-07-26 20:44:01 -0300

    to_deploy_7

--- cambos/core/admin.py
+++ cambos/core/admin.py
@@ -14,16 +14,13 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ['setor']
     list_display = ['username','email']
 
-class OFICINAAdmin(admin.ModelAdmin):
-    list_filter = ['divisao']
-
 
 admin.site.register(User,UserAdmin)
 admin.site.register(Setor)
 admin.site.register(Periodo)
 admin.site.register(Bot)
 admin.site.register(UserBot)
-admin.site.register(OFICINA, OFICINAAdmin)
+admin.site.register(OFICINA)
 admin.site.register(ACABAMENTO)
 
 
