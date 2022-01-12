2fa9adf1da471c57fcdf23460b6a1a6472116d14
JuanCarlosLeonel <juancble94@hotmail.com>
2022-01-12 10:38:32 -0300

    db

---
+++ cambos/cambos/frota/form.py
@@ -0,0 +1,81 @@
+from django import forms
+from .models import Manutencao, Viagem, Abastecimento
+from django_select2.forms import Select2Widget
+
+
+class ViagemForm(forms.ModelForm):
+    class Meta:
+        model = Viagem
+        fields = (            
+            'veiculo',
+            'motorista',            
+            'destino',                        
+            'data_inicial',
+            'hora_inicial',
+            'km_inicial',
+            'data_final',            
+            'hora_final',                        
+            'km_final'
+        )
+         
+        widgets = {                                     
+            'veiculo': forms.HiddenInput(),
+            'motorista': Select2Widget(                
+                attrs={'class':'form-control'},                
+            ),
+            'data_inicial':forms.DateInput(attrs={'data-mask':'00/00/0000','class':'form-control datepicker'}),            
+            'data_final':forms.DateInput(attrs={'class':'form-control datepicker'}),     
+            'hora_inicial': forms.TimeInput(attrs={'data-mask':'00:00','class':'form-control'}),            
+            'hora_final': forms.TimeInput(attrs={'data-mask':'00:00','class':'form-control'}),            
+            'destino': forms.TextInput(attrs={'class':'form-control'}),            
+            'km_inicial': forms.NumberInput(attrs={'class':'form-control'}),
+            'km_final': forms.NumberInput(attrs={'class':'form-control'}),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(ViagemForm, self).__init__(*args, **kwargs)
+        self.fields['data_inicial'].label = "Data Saída"
+        self.fields['hora_inicial'].label = "Hora Saída"
+        self.fields['data_final'].label = "Data Retorno"
+        self.fields['hora_final'].label = "Hora Retorno"
+
+class AbastecimentoForm(forms.ModelForm):
+    class Meta:
+        model = Abastecimento
+        fields = (            
+            'veiculo',
+            'data',
+            'valor_unitario',            
+            'quantidade',            
+        )
+         
+        widgets = {                                     
+            'veiculo': Select2Widget(                
+                attrs={'class':'form-control', 'autofocus': 'autofocus'},                
+            ),            
+            'data': forms.DateInput(attrs={'class':'form-control datepicker'}),
+            'valor_unitario': forms.NumberInput(attrs={'class':'form-control'}),            
+            'quantidade': forms.NumberInput(attrs={'class':'form-control'}),
+        }
+
+class ManutencaoForm(forms.ModelForm):
+    class Meta:
+        model = Manutencao
+        fields = (            
+            'veiculo',
+            'manutencao',
+            'valor',            
+            'descricao', 
+            'data_criacao',           
+        )
+         
+        widgets = {                                     
+            'veiculo': Select2Widget(                
+                attrs={'class':'form-control', 'autofocus': 'autofocus'},                
+            ),            
+            'manutencao': forms.Select(attrs={'class':'form-control'}),
+            'data_criacao': forms.DateInput(attrs={'class':'form-control datepicker'}),
+            'valor': forms.NumberInput(attrs={'class':'form-control'}),            
+            'descricao': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
---
+++ cambos/cambos/frota/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path
+from django.views.generic.base import View
+from .views import(
+    Index,
+    VeiculoIndex,
+    ViagemList,
+    ViagemCreate,
+    ViagemUpdate,
+    ViagemDelete,    
+    AbastecimentoList,
+    AbastecimentoCreate,
+    AbastecimentoUpdate,
+    AbastecimentoDelete,
+    VeiculoList,    
+    ViagemListALL,
+    relatorio_despesa,
+    relatorio_despesa_porveiculo,
+    IndexDespesas,
+    ManutencaoList,
+    ManutencaoCreate,
+    ManutencaoUpdate,
+    ManutencaoDelete,
+)
+
+urlpatterns = [
+    path('index', Index.as_view(), name='frota_index'),        
+    path('veiculo_index/<str:pk>/', VeiculoIndex.as_view(), name='veiculo_index'), 
+    path('viagem_listall',  ViagemListALL.as_view(), name='viagem_listall'),
+    path('viagem_list/<str:pk>/',  ViagemList.as_view(), name='viagem_list'),
+    path('viagem_create/<str:pk>/', ViagemCreate.as_view(), name = 'viagem_create'),
+    path('viagem_update/<str:pk>/', ViagemUpdate.as_view(), name='viagem_update'),    
+    path('viagem_delete/<int:pk>/',ViagemDelete.as_view(), name='viagem_delete_cbv'),
+    path('abastecimento_list',  AbastecimentoList.as_view(), name='abastecimento_list'),
+    path('abastecimento_create/', AbastecimentoCreate.as_view(), name = 'abastecimento_create'),
+    path('abastecimento_update/<str:pk>/', AbastecimentoUpdate.as_view(), name='abastecimento_update'),    
+    path('abastecimento_delete/<int:pk>/',AbastecimentoDelete.as_view(), name='abastecimento_delete_cbv'),
+    path('veiculo_list',  VeiculoList.as_view(), name='veiculo_list'),
+    path('retorna_despesas', relatorio_despesa ,name='retorna_despesas'),
+    path('retorna_despesas_porveiculo', relatorio_despesa_porveiculo ,name='retorna_despesas_porveiculo'),
+    path('despesas_frota', IndexDespesas.as_view(), name='despesas_frota'),
+    path('manutencao_list',  ManutencaoList.as_view(), name='manutencao_list'),
+    path('manutencao_create', ManutencaoCreate.as_view(), name = 'manutencao_create'),
+    path('manutencao_update/<str:pk>/', ManutencaoUpdate.as_view(), name='manutencao_update'),    
+    path('manutencao_delete/<int:pk>/',ManutencaoDelete.as_view(), name='manutencao_delete_cbv'),
+]
+    
